b7df2a641d50e7483f1069a9bc1868bfe19877e8
maoguala <danny338898@gmail.com>
2026-09-12 15:24:13 +0800

    HelloWorld with OOP concept(Encapsulation)

---
+++ HelloWorld.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+class talk{
+    private:
+        std::string sentence = "Hello World!\n";
+    public:
+        
+    //Getter
+    const std::string& GetSentence() const{
+        return sentence;
+    }
+
+    //Setter
+    void SetSentence(const std::string& SetSentence)
+    {
+        sentence = SetSentence;
+    }
+
+};
+
+
+int main(){
+    talk t;
+    std::cout << t.GetSentence();
+}
